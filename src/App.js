2570700a2f67422be072d14d970fa79cc49ea2a5
Sanjida Islam <sanjimo123@gmail.com>
2021-10-02 20:11:46 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
+import ManageInventory from './components/ManageInventory/ManageInventory';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Header></Header>
+        <Switch>
+            <Route exact path='/shop'><Shop></Shop></Route>
+            <Route exact path='/'><Shop></Shop></Route>
+            <Route exact path='/review'><OrderReview></OrderReview></Route>
+            <Route exact path='/inventory'><ManageInventory></ManageInventory></Route>
+            <Route path='*'><NotFound></NotFound></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
-    const {img,name,price,stock,seller}=props.product;
+    const {img,name,price,seller}=props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
@@ -15,7 +15,6 @@ const Product = (props) => {
              <h4 className="product-name">{name}</h4>
              <p><small>by : {seller}</small></p>
              <p>Price : ${price}</p>
-             <p><small>Only {stock} left in stock - order soon </small></p>
              <button
              onClick={()=>props.handleAddToCart(props.product)}
              className="btn-regular"
